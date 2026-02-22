f228dca24be575b7a0cd6be254743674c80fd48b
Wit <twoj.mail@domena.pl>
2026-02-22 12:19:53 +0100

    frontend: fix Authorization header bearer token

--- digital_signage/frontend/src/services/api.ts
+++ digital_signage/frontend/src/services/api.ts
@@ -14,7 +14,7 @@ api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token')
     if (token) {
-      config.headers.Authorization = Bearer 
+      config.headers.Authorization = `Bearer ${token}`
     }
     return config
   },
